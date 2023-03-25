e15a63e00d9f528edc0b7c850a4bb66f81ba4ae2
Schow94 <Schow94@gmail.com>
2023-03-25 14:14:09 -0700

    Hookup client to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+const API_URL = `http://ec2-3-128-198-148.us-east-2.compute.amazonaws.com:5001`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+
+	const getData = async () => {
+		const data = await axios.get(`${API_URL}/users`);
+
+		setUsers(data.data.users);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const renderUsers = () => {
+		return users.map((u) => {
+			return (
+				<div key={u.user_ID}>
+					<p>
+						{u.firstname} {u.lastname} - {u.email}
+					</p>
+				</div>
+			);
+		});
+	};
+
+	return (
+		<div className="App">
+			<h1>This entire app is hosted on AWS</h1>
+			<p>Frontend: AWS Amplify</p>
+			<p>Backend: AWS EC2</p>
+			<p>MySQL: AWS RDS</p>
+
+			<div>
+				<h2>Users Table from MySQL</h2>
+
+				{renderUsers()}
+			</div>
+		</div>
+	);
 }
 
 export default App;
